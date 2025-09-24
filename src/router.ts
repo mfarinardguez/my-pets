c3f217c0bd1ce7c70580891751c4ade7ccf9aace
María Candelaria Fariña Rodríguez <mfarinardguez@gmail.com>
2025-09-24 23:15:09 +0100

    feat(router.ts): Create router file

---
+++ src/router.ts
@@ -0,0 +1,10 @@
+import { Router } from "@vaadin/router";
+
+export function initRouter() {
+  const outlet = document.querySelector('#outlet');
+  const router = new Router(outlet);
+
+  router.setRoutes([
+    { path: '/', component: 'dashboard-page' },
+  ])
+}
