0f1a047393463356e4cbf650a11c39764e7635eb
María Candelaria Fariña Rodríguez <mfarinardguez@gmail.com>
2025-09-24 23:44:30 +0100

    feat(router.ts): If a user is logged in, the page will take him to the dashboard page, if not, to the login page

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,20 @@
 import { Router } from "@vaadin/router";
+import { auth } from "./firebase";
 
 export function initRouter() {
   const outlet = document.querySelector('#outlet');
   const router = new Router(outlet);
 
+  const guard = async (_context: any, commands: any) => {
+    const user = auth.currentUser;
+    if (!user) {
+      return commands.redirect('/login');
+    }
+    return undefined;
+  }
+
   router.setRoutes([
-    { path: '/', component: 'dashboard-page' },
+    { path: '/', component: 'dashboard-page', action: guard },
     { path: '/login', component: 'login-page' },
   ])
 }
